6f2454f1a3252d069eb79c21932569daea5df87e
Tom O'Connor <murphic@Thomass-MacBook-Pro.local>
2014-01-23 16:21:03 +0000

    added sign up to site

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Onboard::Application.routes.draw do
   
+  
   # this links urls with your rooms controller so you need 
   # to use the same words
   resources :rooms
+
+# sign up our users to the site
+  resources :users
 
   # go to homepage. This translates root as being homepage (index)
   root "rooms#index"
