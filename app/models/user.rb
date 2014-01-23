6aa2c323ced53fc36bedd9ad25ebb401105556e3
Tom O'Connor <murphic@Thomass-MacBook-Pro.local>
2014-01-23 17:03:57 +0000

    added login and logout

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,6 @@ class User < ActiveRecord::Base
 	validates :username, presence: true, uniqueness: true 
 	validates :email, presence: true, uniqueness: true
 
+#  can add many more if we want
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Onboard::Application.routes.draw do
 # sign up our users to the site
   resources :users
 
+  # log in and log out of our site
+  # session is in singular whereas the others
+  # are plural because you can only do one session at a time
+
+  resource :session 
+
   # go to homepage. This translates root as being homepage (index)
   root "rooms#index"
 
